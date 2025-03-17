e7b3a61e2aa881e97fab0031574c72df1ff222b6
temesgentesfazgi <natitesfazgi@gmail.com>
2025-03-17 15:05:02 -0700

    Updated package

---
+++ src/main/java/com/github/temesgentesfazgi/calculator/Main.java
@@ -0,0 +1,12 @@
+package com.github.temesgentesfazgi.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(5, 3);
+        System.out.println("5 + 3 = " + sum);
+
+    }
+}
+
